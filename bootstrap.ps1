058d5b49658e2eefc387ea374ee56a36e836253b
shskwmt <shskwmt@outlook.jp>
2024-10-08 10:44:58 +0900

    feat: add findutils

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,6 +1,6 @@
 # install packages
 scoop bucket add extras
-scoop install git openssh grep ripgrep emacs gh make
+scoop install git openssh grep ripgrep emacs gh make findutils
 
 # Copy .gitconfig
 Copy-Item -Path ./.gitconfig -Destination ~/
