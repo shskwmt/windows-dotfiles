ad2700c2a5509cd827747bda8666935801e136e7
shskwmt <shskwmt@outlook.jp>
2024-10-10 21:28:49 +0900

    feat: add fd

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,6 +1,6 @@
 # install packages
 scoop bucket add extras
-scoop install git openssh grep ripgrep emacs gh make findutils terraform ollama
+scoop install git openssh grep ripgrep emacs gh make findutils terraform ollama fd
 
 # Copy .gitconfig
 Copy-Item -Path ./.gitconfig -Destination ~/
