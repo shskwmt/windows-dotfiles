08f265b2deb83e1dd530f3c5bd50895816c70099
shskwmt <shskwmt@outlook.jp>
2024-10-10 13:14:26 +0900

    feat: add terraform

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,6 +1,6 @@
 # install packages
 scoop bucket add extras
-scoop install git openssh grep ripgrep emacs gh make findutils
+scoop install git openssh grep ripgrep emacs gh make findutils terraform
 
 # Copy .gitconfig
 Copy-Item -Path ./.gitconfig -Destination ~/
