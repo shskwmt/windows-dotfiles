3f53031431110c5ee73adc060178b42e90321c0e
shskwmt <shskwmt@outlook.jp>
2024-10-11 00:38:42 +0900

    feat: add anaconda3

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,6 +1,6 @@
 # install packages
 scoop bucket add extras
-scoop install git openssh grep ripgrep emacs gh make findutils terraform ollama fd
+scoop install git openssh grep ripgrep emacs gh make findutils terraform ollama fd anaconda3
 
 # Copy .gitconfig
 Copy-Item -Path ./.gitconfig -Destination ~/
