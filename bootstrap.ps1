3f82bccaa8c2274d85ac228b955e51c5e90b0f69
shskwmt <shskwmt@outlook.jp>
2024-10-20 00:16:48 +0900

    feat: add font and lsp

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,6 +1,25 @@
 # install packages
 scoop bucket add extras
-scoop install git openssh grep ripgrep emacs gh make findutils terraform ollama fd anaconda3
+scoop bucket add nerd-fonts
+
+# data science
+scoop install ollama anaconda3
+
+# cli tools
+scoop install git openssh grep ripgrep gh make findutils fd msys2
+
+# font
+scoop install Noto-NF-Mono NerdFontsSymbolsOnly
+
+# programming
+scoop install emacs terraform go ruby
+
+# install gopls
+go install golang.org/x/tools/gopls@latest
+
+# install ruby-lsp
+ridk install 3
+gem install ruby-lsp
 
 # Copy .gitconfig
 Copy-Item -Path ./.gitconfig -Destination ~/
